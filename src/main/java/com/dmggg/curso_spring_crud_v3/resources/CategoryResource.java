c0f96b937faedde13416d893e0e905aaa13a55b0
DiegoDMG28 <vernoschi2@gmail.com>
2026-06-07 18:11:59 -0300

    começo do postmapping

--- src/main/java/com/dmggg/curso_spring_crud_v3/resources/CategoryResource.java
+++ src/main/java/com/dmggg/curso_spring_crud_v3/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.dmggg.curso_spring_crud_v3.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dmggg.curso_spring_crud_v3.dto.CategoryDTO;
 import com.dmggg.curso_spring_crud_v3.services.CategoryService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -30,4 +35,11 @@ public class CategoryResource {
   public ResponseEntity<CategoryDTO> find_by_id(@PathVariable long id){
     return ResponseEntity.ok().body(service.find_by_id(id));
   }
+
+  @PostMapping(value = "/add/{dto}")
+  public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path()
+    dto = insert(dto);
+  }
+  
 }
